8d812a1a1d8b416dcb57d6dd1dbbe929366df814
Robert <roberto.huayhua@tecsup.edu.pe>
2024-09-11 09:04:20 -0500

    Add  SurfaceExample

--- app/src/main/java/com/example/nuevoproyecto4/MainActivity.kt
+++ app/src/main/java/com/example/nuevoproyecto4/MainActivity.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.nuevoproyecto4.ui.theme.NuevoProyecto4Theme
@@ -29,6 +31,7 @@ class MainActivity : ComponentActivity() {
                     )
                     LazyColumnExample()
                     LazyRowExample()
+                    SurfaceExample()
                 }
             }
         }
@@ -64,10 +67,6 @@ fun LazyRowExample() {
 }
 
 
-
-
-
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -76,3 +75,13 @@ fun GreetingPreview() {
     }
 }
 
+@Composable
+fun SurfaceExample() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.Blue,
+        contentColor = Color.White
+    ) {
+        Text("This is a Surface", modifier = Modifier.padding(16.dp))
+    }
+}
